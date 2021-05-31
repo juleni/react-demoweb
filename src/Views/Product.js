0014f578d90038baedaffb70d8dc2607faa6e16e
juleni <69912179+juleni@users.noreply.github.com>
2021-05-31 13:18:51 +0200

    added ErrorMessage

--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,42 +1,19 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
+import ErrorMessage from '../Components/ErrorMessage';
 import Loader from "../Components/Loader";
+import { useAxiosGet } from "../Hooks/HTTPRequest";
 
 function Product() {
 
   const { id } = useParams();
   const url = `https://60b11d181f26610017fffe7b.mockapi.io/api/v1/products/${id}`;
-  const [product, setProduct] = useState(
-    {loading: false,
-        data: null,
-       error: false,
-    });
+
+  let product = useAxiosGet(url)
 
   let content = null;
 
-  useEffect(() => {
-      setProduct({loading: true,
-                     data: null,
-                    error: false});
-      axios.get(url).then(response => {
-        setProduct({loading: true,
-                       data: response.data,
-                      error: false});
-      })
-      .catch(() => {
-        setProduct({loading: false,
-                       data: null,
-                      error: true});
-      })
-    }, [url]
-  )
-
-  
   if (product.error) {
-    content = <p>
-      There was an error. Please refresh or try again later.
-    </p>
+    content = <ErrorMessage clColor="red" message="There was an error. Please refresh or try again later."/>
   }
   
   if (product.loading) {
