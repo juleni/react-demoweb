2afca1dcbe839b06ab9b63e09294f16efd026548
juleni <69912179+juleni@users.noreply.github.com>
2021-05-31 11:30:55 +0200

    Added new component

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,11 +1,60 @@
-import HelloWorld from '../Components/HelloWorld';
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Loader from "../Components/Loader";
+import ProductCard from "../Components/ProductCard";
 
 function Home() {
+  const url = `https://60b11d181f26610017fffe7b.mockapi.io/api/v1/products?page=1&limit=10`;
+  const [products, setProducts] = useState(
+    {loading: false,
+        data: null,
+       error: false,
+    });
+
+  let content = null;
+  useEffect(() => {
+    setProducts({loading: true,
+                   data: null,
+                  error: false});
+    axios.get(url).then(response => {
+      setProducts({loading: true,
+                     data: response.data,
+                    error: false});
+    })
+    .catch(() => {
+      setProducts({loading: false,
+                     data: null,
+                    error: true});
+    })
+  }, [url]
+  ) 
+
+  if (products.error) {
+    content = <p>
+      There was an error. Please refresh or try again later.
+    </p>
+  }
+    
+  if (products.loading) {
+    content = <Loader />
+  }
+
+  if (products.data) {
+    content = 
+      products.data.map((product,) => 
+        <div key={ product.id }>
+          <ProductCard product={ product }/>
+        </div>
+        
+    )
+  } 
+
   return(
     <div>
-      <h1 className="font-bold text-2xl mb-3">This is homepage</h1>
+      <h1 className="font-bold text-2xl mb-3">Best Sellers</h1>
 
-      <HelloWorld name="Juleni"/>
+      { content }
     </div>
   );
 }
--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,43 +1,69 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
+import Loader from "../Components/Loader";
 
 function Product() {
 
   const { id } = useParams();
   const url = `https://60b11d181f26610017fffe7b.mockapi.io/api/v1/products/${id}`;
-  const [product, setProduct] = useState(null);
+  const [product, setProduct] = useState(
+    {loading: false,
+        data: null,
+       error: false,
+    });
 
   let content = null;
 
   useEffect(() => {
+      setProduct({loading: true,
+                     data: null,
+                    error: false});
       axios.get(url).then(response => {
-        setProduct(response.data);
-      });
+        setProduct({loading: true,
+                       data: response.data,
+                      error: false});
+      })
+      .catch(() => {
+        setProduct({loading: false,
+                       data: null,
+                      error: true});
+      })
     }, [url]
   )
 
-  if (product) {
-    return(
-      content = 
+  
+  if (product.error) {
+    content = <p>
+      There was an error. Please refresh or try again later.
+    </p>
+  }
+  
+  if (product.loading) {
+    content = <Loader />
+  }
+
+  if (product.data) {
+    content = 
+      <div>
+        <h1 className="text-2xl font-bold mb-3">
+          { product.data.name }
+        </h1>
         <div>
-          <h1 className="text-2xl font-bold mb-3">
-            { product.name }
-          </h1>
-          <div>
-            <img src={ product.images[0].imageUrl }
-                 alt={ product.name }
-            />
-          </div>
-          <div className="font-bold text-xl mb-3">
-            $ { product.price }
-          </div>
-          <div>
-            { product.description }
-          </div>
+          <img src={ product.data.images[0].imageUrl }
+                alt={ product.data.name }
+          />
         </div>
-    )
-  } else return (
+        <div className="font-bold text-xl mb-3">
+          $ { product.data.price }
+        </div>
+        <div>
+          { product.data.description }
+        </div>
+      </div>
+  } 
+  
+  return (
     <div> { content } </div>
   )
 }
